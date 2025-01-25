0ac8e2f61dda3dedf1006f06deb5651ead1b7ac8
Ivan Xiao <me@ivanxiao.com>
2025-01-24 19:56:10 -0800

    Util function to print label array as string

--- src/MusicBrainz/Label.php
+++ src/MusicBrainz/Label.php
@@ -73,4 +73,13 @@ class Label
     public static function fromArray(array $labelInfo, MusicBrainz $brainz) {
         return array_map(fn($info) => new Artist($info['label'], $brainz), $labelInfo);
     }
+
+    /**
+     * Util function to implode Label[] as a string
+     * @return string
+     */ 
+    public static function arrayToString(array $labels, string $concat = ", ") {
+        $labels = array_map(fn($label) => $label->name, $labels);
+        return implode($concat, $labels);
+    }
 }
